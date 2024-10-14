c86ff4d0bd9a7077eb51a8f30e7f8b4aaf147b7a
rognit <pierre.brg314@gmail.com>
2024-10-14 20:00:12 +0200

    Working set functional

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from SkipList import SkipList
 from ToDoList import ToDoList
+from WorkingToDoList import WorkingToDoList
 
 def test_skiplist():
     skiplist = SkipList(max_level=3, p=0.5, verbose=True)
@@ -40,6 +41,33 @@ def test_todolist():
     print(todolist)
 
 
+def test_working_todolist():
+    # Create an instance of WorkingToDoList
+    working_todolist = WorkingToDoList(h=11, epsilon=0.2, verbose=True)
+
+    # Insert a few keys
+    print("Inserting keys:")
+    for i in range(10000):  # Inserting 20 keys (0-19)
+        working_todolist.insert(i)
+    print(working_todolist)
+
+    # Search for some keys
+    print("Searching for keys:")
+    working_todolist.search(5)
+    working_todolist.search(10)
+    working_todolist.search(15)
+    working_todolist.search(25)  # This should not be found
+
+    # Delete a few keys
+    print("Deleting keys:")
+    working_todolist.delete(3)
+    working_todolist.delete(7)
+    working_todolist.delete(15)
+
+    print(working_todolist)
+
+
 if __name__ == "__main__":
     #test_skiplist()
-    test_todolist()
+    #test_todolist()
+    test_working_todolist()
